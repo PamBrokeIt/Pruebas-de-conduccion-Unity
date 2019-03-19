1d065f095527115eba88ad6a75200bba951f5a71
Manu <mouzomarsella@gmail.com>
2019-03-19 19:03:45 +0100

    Avanzados scripts de conducir y suspension

--- Assets/Codigo/Conducir.cs
+++ Assets/Codigo/Conducir.cs
@@ -5,33 +5,85 @@ using UnityEngine;
 public class Conducir : MonoBehaviour
 {
 
-    public float aceleracion = 100f;
+    /*
+    public float aceleracion = 20f;
     public float velocidadMax = 100f;
     public Rigidbody coche;
+    */
 
-    // Start is called before the first frame update
+    //--------RAYCAST--------
+    public float hoverForce = 1000;
+    public float gravityForce = 1000f;
+    public float hoverHeight = 1.5f;
+    //--------RAYCAST--------
+
+    //--------MOVEMENT--------
+    float deadZone = 0.1f;
+
+    public float groundedDrag = 3f;
+
+    public float maxVelocity = 50;
+
+    public float forwardAcceleration = 8000f;
+    public float reverseAcceleration = 4000f;
+    float thrust = 0f;
+
+    public float turnStrength = 1000f;
+    float turnValue = 0f;
+    //--------MOVEMENT--------
+
+    //--------GAMEOBJECTS--------
+    int layerMask;
+    Rigidbody body;
+
+    public GameObject[] hoverPoints;
+
+    public ParticleSystem[] dustTrails = new ParticleSystem[2];
+
+    public Transform[] wheelTransform = new Transform[4]; //these are the transforms for our 4 wheels
+
+    // the physical transforms for the car's wheels
+    public Transform frontLeftWheel;
+    public Transform frontRightWheel;
+    public Transform rearLeftWheel;
+    public Transform rearRightWheel;
+    //--------GAMEOBJECTS--------
+
+
+
     void Start()
     {
-        coche = GetComponent<Rigidbody>();
+        //coche = GetComponent<Rigidbody>();
 
+        //--------GAMEOBJECTS--------
+        body = GetComponent<Rigidbody>();
+        body.centerOfMass = Vector3.down; //UNO POR DEBAJO DEL VEHICULO
+
+        layerMask = 1 << LayerMask.NameToLayer("Vehicle");
+        layerMask = ~layerMask;
+        //--------GAMEOBJECTS--------
 
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+
     }
 
     void FixedUpdate()
     {
-
-        Vector3 fuerza = new Vector3(0.0f, 1.0f, 0.0f); //aplicar fuerza hacia arriba.
+        /*
+ 
+        //Vector3 fuerza = new Vector3(0.0f, 1.0f, 0.0f); //aplicar fuerza hacia arriba.
+        Vector3 fuerza = new Vector3(0.0f, 0.0f, 1.0f); //aplicar fuerza hacia delante.
 
         var velVector = coche.velocity; //recoge la velocidad del rigidbody en forma de vector
         float velActual = velVector.magnitude; //recoge la longitud del vector velocidad (el modulo)
 
-        Debug.Log(aceleracion);
+        Debug.Log(velVector);
+
+        //Debug.Log(velActual);
 
         if (Input.GetKey(KeyCode.W))
         {
@@ -40,6 +92,81 @@ public class Conducir : MonoBehaviour
                 coche.AddForce(fuerza * aceleracion, ForceMode.Impulse);
             }
         }
+        */
+
+
+        //--------MOVEMENT--------
+        // Main Thrust
+        thrust = 0.0f;
+        float acceleration = Input.GetAxis("Vertical");
+
+
+
+        if (acceleration > deadZone)
+            thrust = acceleration * forwardAcceleration;
+        else if (acceleration < -deadZone)
+            thrust = acceleration * reverseAcceleration;
+
+        // Turning
+        turnValue = 0.0f;
+        float turnAxis = Input.GetAxis("Horizontal");
+        if (Mathf.Abs(turnAxis) > deadZone)
+            turnValue = turnAxis;
+        //--------MOVEMENT--------
+
+
+
+        //--------MOVEMENT--------
+
+        //var emissionRate = 0;
+        Suspension rueda;
+        rueda = gameObject.GetComponentInChildren<Suspension>();
+
+        if (rueda.grounded)
+        {
+            body.drag = groundedDrag;
+            //emissionRate = 10;
+        }
+        else
+        {
+            body.drag = 0.1f;
+            thrust /= 100f;
+            turnValue /= 100f;
+        }
+
+        for (int i = 0; i < dustTrails.Length; i++)
+        {
+            //var emission = dustTrails[i].emission;
+            //emission.rate = new ParticleSystem.MinMaxCurve(emissionRate);
+        }
+        //--------MOVEMENT--------
+
+
+        //--------MOVEMENT--------
+
+        // Handle Forward and Reverse forces
+        Debug.Log(Mathf.Abs(thrust));
+
+        if (Mathf.Abs(thrust) > 0)
+        {
+            body.AddForce(transform.forward * thrust);
+        }
+        // Handle Turn forces
+        if (turnValue > 0)
+        {
+            body.AddRelativeTorque(Vector3.up * turnValue * turnStrength);
+        }
+        else if (turnValue < 0)
+        {
+            body.AddRelativeTorque(Vector3.up * turnValue * turnStrength);
+        }
+
+        // Limit max velocity
+        if (body.velocity.sqrMagnitude > (body.velocity.normalized * maxVelocity).sqrMagnitude)
+        {
+            body.velocity = body.velocity.normalized * maxVelocity;
+        }
+        //--------MOVEMENT--------
 
     }
 }
---
+++ Assets/Codigo/Suspension.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Suspension : MonoBehaviour
+{
+    private Rigidbody coche;
+    private Transform rueda; //posicion de la rueda alrededor del coche (local es el coche)
+
+    public float gravityForce = 1000f;
+    public float radioRueda = 1.0f;
+    public float suspLongMaxima = 0.2f;
+    public float hoverForce = 1000;
+    public float hoverHeight;
+    float thrust = 0f;
+    public bool grounded;
+
+    public GameObject hoverPoint;
+
+
+    // Works like start but before it
+
+    void Awake()
+    {
+        coche = transform.root.GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {        
+        Vector3 ralloOrigen = rueda.position;
+        hoverHeight = suspLongMaxima + radioRueda;
+    }
+
+
+    private void FixedUpdate()
+    {
+        float speed = coche.velocity.magnitude;
+
+
+        //--------RAYCAST--------
+        //  Hover Force
+        RaycastHit hit;
+        bool grounded = false;
+
+        if (Physics.Raycast(hoverPoint.transform.position, -Vector3.up, out hit, hoverHeight))
+        {
+            coche.AddForceAtPosition(hoverPoint.transform.up * gravityForce, hit.point);
+            grounded = true;
+        }
+        else
+        {
+            // Self levelling - returns the vehicle to horizontal when not grounded
+            if (transform.position.y > hoverPoint.transform.position.y)
+            {
+                coche.AddForceAtPosition(hoverPoint.transform.up * gravityForce, hoverPoint.transform.position);
+            }
+            else
+            {
+                coche.AddForceAtPosition(hoverPoint.transform.up * -gravityForce, hoverPoint.transform.position);
+            }
+        }
+
+
+        /*
+            if (Physics.Raycast(hoverPoint.transform.position, -Vector3.up, out hit, ))
+            {
+                coche.AddForceAtPosition(Vector3.up * hoverForce * (1.0f - (hit.distance / hoverHeight)), hoverPoint.transform.position);
+                grounded = true;
+                Debug.DrawRay(transform.position, -Vector3.up, Color.red);
+            }
+            else
+            {
+                // Self levelling - returns the vehicle to horizontal when not grounded
+                if (transform.position.y > hoverPoint.transform.position.y)
+                {
+                    body.AddForceAtPosition(hoverPoint.transform.up * gravityForce, hoverPoint.transform.position);
+                }
+                else
+                {
+                    body.AddForceAtPosition(hoverPoint.transform.up * -gravityForce, hoverPoint.transform.position);
+                }
+            }
+            /*
+
+        //--------RAYCAST--------
+
+
+
+        // down = local downwards direction
+        //Vector3 down = transform.TransformDirection(Vector3.down);
+
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, radioRueda + suspLongMaxima))
+        {
+
+            grounded = true;
+            // the velocity at point of contact
+            //Vector3 velocityAtTouch = parent.GetPointVelocity(hit.point);
+
+            // calculate spring compression
+            // difference in positions divided by total suspension range
+            float compression = hit.distance / (suspLongMaxima + radioRueda);
+            compression = -compression + 1;
+
+            // final force
+            //Vector3 force = Vector3.down * compression * spring;
+            // velocity at point of contact transformed into local space
+
+            // Vector3 t = transform.InverseTransformDirection(velocityAtTouch);
+
+            // local x and z directions = 0
+            //t.z = t.x = 0;
+
+            // back to world space * -damping
+            //Vector3 damping = transform.TransformDirection(t) * -damper;
+            Vector3 finalForce = force;
+
+            // VERY simple turning - force rigidbody in direction of wheel
+            /*t = parent.transform.InverseTransformDirection(velocityAtTouch);
+            t.y = 0;
+            t.z = 0;
+
+            t = transform.TransformDirection(t);
+            */
+
+
+
+    }
+}
